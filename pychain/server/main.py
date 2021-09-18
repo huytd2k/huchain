b0b50af3397a38bcc77ba2dd8d0ea06e713fa92d
Huy tran <huy.td183768@sis.hust.edu.vn>
2021-09-18 22:03:47 +0700

    add server base methods

--- pychain/server/main.py
+++ pychain/server/main.py
@@ -1,15 +1,28 @@
+from pychain.core.chain import Chain
 from typing import Optional
 
 from fastapi import FastAPI
+from uuid import uuid4
 
 app = FastAPI()
 
+# Generate the node ID
+node_id = str(uuid4()).replace("-", "")
+# Create blockchain
+chain = Chain()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+
+@app.get("/mine")
+def mine():
+    return "Mining..."
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/transactions")
+def new_trx(sender: str, recipient: str, amount: float):
+    block_index = chain.new_transaction(sender, recipient, amount)
+    return {"result": f"This transactions will be added to block {block_index}"}
+
+
+@app.get("/chain")
+def get_chain():
+    return {"chain": chain.chain, "length": len(chain)}
